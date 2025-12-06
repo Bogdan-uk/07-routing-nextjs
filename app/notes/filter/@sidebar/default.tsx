e45969503dce35dd0c8a54b0a2d9a2e6062e1e87
Bogdan-uk <656olololol@gmail.com>
2025-12-06 10:12:28 +0200

    view

---
+++ app/notes/filter/@sidebar/default.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import css from './SidebarNotes.module.css';
+
+const tags = ['Work', 'Personal', 'Ideas', 'Important'];
+
+export default function SidebarNotes() {
+  return (
+    <ul className={css.menuList}>
+      <li className={css.menuItem}>
+        <Link href="/notes/filter/all" className={css.menuLink}>
+          All notes
+        </Link>
+      </li>
+      {tags.map(tag => (
+        <li key={tag} className={css.menuItem}>
+          <a href={`/notes/filter/${tag}`} className={css.menuLink}>
+            {tag}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
